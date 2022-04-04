c1eb4659d93ce6906983a28953bae43724572968
vaishnav.gade <vaishnav.gade@gmail.com>
2022-04-04 02:06:38 -0400

    MVC for being able to make graphql queries

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
-import {execSync} from 'child_process';
+import { execSync } from 'child_process';
+import { graphql } from '@octokit/graphql';
+
+const gitHubPAT = '';
 
 // console.log(`
 
@@ -9,7 +12,7 @@ import {execSync} from 'child_process';
 // ██║     ██╔══██╗██╔══╝  ██╔══██║   ██║   ██╔══╝      ██╔═══╝ ██╔══██╗
 // ╚██████╗██║  ██║███████╗██║  ██║   ██║   ███████╗    ██║     ██║  ██║
 //  ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝    ╚═╝     ╚═╝  ╚═╝
-                                                                     
+
 // `);
 
 let gitURL = '';
@@ -17,15 +20,69 @@ try {
     gitURL = execSync('git config --get remote.origin.url');
     console.log(`Git remote url: ${gitURL}`);
 }
-catch(error) {
+catch (error) {
     console.error(`The current directory might not be a git repo. ${error}`);
-    process.exit(1) //mandatory (as per the Node.js docs)
+    process.exit(1) // mandatory (as per the Node.js docs)
 }
 
 let splitURL = new String(gitURL).split(':');
-let orgPlusRepo = splitURL[1].split('/');
-const org = orgPlusRepo[0];
-const repo = orgPlusRepo[1].split('.')[0];
+let ownerPlusRepo = splitURL[1].split('/');
+const owner = ownerPlusRepo[0];
+const repo = ownerPlusRepo[1].split('.')[0];
 
-console.log(org);
+console.log(owner);
 console.log(repo);
+
+const graphqlWithAuth = graphql.defaults({
+    baseUrl: "https://api.github.com",
+    headers: {
+        authorization: `Bearer ${gitHubPAT}`,
+    },
+});
+
+// const repository = await graphqlWithAuth(`
+// {
+//     viewer {
+//         login
+//     }
+// }
+// `);
+
+// const query = `{
+//     repository(owner: "octokit", name: "graphql.js") {
+//       issues(last: 3) {
+//         edges {
+//           node {
+//             title
+//           }
+//         }
+//       }
+//     }
+//   }`;
+
+
+// const { repository } = await graphqlWithAuth(query);
+// console.log(JSON.stringify(repository));
+
+const query = `
+query lastIssues($owner: String!, $repo: String!, $num: Int = 3) {
+  repository(owner: $owner, name: $repo) {
+    issues(last: $num) {
+      edges {
+        node {
+          title
+        }
+      }
+    }
+  }
+}
+`;
+
+const lastIssues = await graphqlWithAuth(query,
+    {
+        owner: `${owner}`,
+        repo: `${repo}`
+    }
+);
+
+console.log(JSON.stringify(lastIssues));
