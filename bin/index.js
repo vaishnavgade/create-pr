4f4f768a7b0d29b13fc7c0aebc962ea738d60da3
vaishnav.gade <vaishnav.gade@gmail.com>
2022-04-04 00:26:27 -0400

    Parsing the git url for org and repo name

--- bin/index.js
+++ bin/index.js
@@ -1,23 +1,31 @@
 #!/usr/bin/env node
 import {execSync} from 'child_process';
 
-console.log(`
+// console.log(`
 
-██████╗██████╗ ███████╗ █████╗ ████████╗███████╗    ██████╗ ██████╗ 
-██╔════╝██╔══██╗██╔════╝██╔══██╗╚══██╔══╝██╔════╝    ██╔══██╗██╔══██╗
-██║     ██████╔╝█████╗  ███████║   ██║   █████╗      ██████╔╝██████╔╝
-██║     ██╔══██╗██╔══╝  ██╔══██║   ██║   ██╔══╝      ██╔═══╝ ██╔══██╗
-╚██████╗██║  ██║███████╗██║  ██║   ██║   ███████╗    ██║     ██║  ██║
- ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝    ╚═╝     ╚═╝  ╚═╝
+// ██████╗██████╗ ███████╗ █████╗ ████████╗███████╗    ██████╗ ██████╗ 
+// ██╔════╝██╔══██╗██╔════╝██╔══██╗╚══██╔══╝██╔════╝    ██╔══██╗██╔══██╗
+// ██║     ██████╔╝█████╗  ███████║   ██║   █████╗      ██████╔╝██████╔╝
+// ██║     ██╔══██╗██╔══╝  ██╔══██║   ██║   ██╔══╝      ██╔═══╝ ██╔══██╗
+// ╚██████╗██║  ██║███████╗██║  ██║   ██║   ███████╗    ██║     ██║  ██║
+//  ╚═════╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝    ╚═╝     ╚═╝  ╚═╝
                                                                      
-`);
+// `);
 
+let gitURL = '';
 try {
-    const gitData = execSync('git config --get remote.origin.url');
-    console.log(`Git remote url: ${gitData}`);
+    gitURL = execSync('git config --get remote.origin.url');
+    console.log(`Git remote url: ${gitURL}`);
 }
 catch(error) {
     console.error(`The current directory might not be a git repo. ${error}`);
     process.exit(1) //mandatory (as per the Node.js docs)
 }
 
+let splitURL = new String(gitURL).split(':');
+let orgPlusRepo = splitURL[1].split('/');
+const org = orgPlusRepo[0];
+const repo = orgPlusRepo[1].split('.')[0];
+
+console.log(org);
+console.log(repo);
